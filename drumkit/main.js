44c116f40736e63046509aabbf998914339edd97
Marlon Braga <rdg.brg@gmail.com>
2019-04-25 22:49:02 -0400

    Day-1 Drumkit-2

--- drumkit/main.js
+++ drumkit/main.js
@@ -1,20 +1,15 @@
 function playSound(e) {
-  // console.log(e.keyCode);
-  // console.log("current time: " + e.currentTime);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  // console.log(key);
-  // console.log(audio);
+
   if(!audio) return; // This will stop the function from running all together.
   audio.currentTime = 0;  // Rewind to the start
   audio.play();
   key.classList.add('playing');
 }
 function removeTransition(e) {
-  // console.log(e)
   if(e.propertyName !== 'transform') return; // Skip it if it's not a transform
-  // console.log(e.propertyName);
-  // console.log(this);
+
   this.classList.remove('playing');
 }
 
