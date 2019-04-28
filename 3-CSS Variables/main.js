910902a01a28a6510f17b77d40edf2a04ff835a0
Marlon Braga <rdg.brg@gmail.com>
2019-04-28 17:02:16 -0400

    Day-3 personal touches-2

--- 3-CSS Variables/main.js
+++ 3-CSS Variables/main.js
@@ -1,9 +1,10 @@
 const inputs = document.querySelectorAll('.controls input');
-
+let kamehameha = document.querySelector('.energyBall');
 
 function handleUpdate() {
   const suffix = this.dataset.sizing || '';
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  kamehameha.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
