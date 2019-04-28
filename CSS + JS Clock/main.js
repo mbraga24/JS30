ff0f6ce415e0e95e5a43e0b391fa146df38ecb50
Marlon Braga <rdg.brg@gmail.com>
2019-04-28 09:13:12 -0400

    Day-2 CSS JS CLOCK-2

--- CSS + JS Clock/main.js
+++ CSS + JS Clock/main.js
@@ -8,18 +8,14 @@ function setDate() {
   const seconds = now.getSeconds();
   const secondsDegrees = (((seconds / 60) * 360) + 90);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  // console.log(seconds);
 
   const minutes = now.getMinutes();
-  const minutesDegrees = (((minutes / 60) * 360) + 90);
+  const minutesDegrees = ((minutes + (seconds / 60)) / 60 * 360) + 90;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-  // console.log(minutes);
 
   const hours = now.getHours();
-  const hoursDegrees = (((hours / 12) * 360) + 90);
+  const hoursDegrees = (( hours + ( minutes / 60 ) + ( seconds / 3600 ) ) / 12 * 360) + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
-
 }
 
 setInterval(setDate, 1000);
