3b46df2a543ed6451e95d6f27bea2e3b235c6392
Marlon Braga <rdg.brg@gmail.com>
2019-04-30 01:14:32 -0400

    Array cardio

--- 4-Array Cardio/main.js
+++ 4-Array Cardio/main.js
@@ -48,14 +48,21 @@ const inventors = [
 
     // Array.prototype.reduce()
     // 4. How many years did all the inventors live?
-      let totalYears = inventors.reduce((total, inventor) => {
-        return total + (inventor.passed - inventor.year)
-      }, 0);
+      // let totalYears = inventors.reduce((total, inventor) => {
+      //   return total + (inventor.passed - inventor.year)
+      // }, 0);
 
-      console.log(totalYears);
+      // console.log(totalYears);
 
     // 5. Sort the inventors by years lived
+      const oldest = inventors.sort((a,b) => {
+        const inventorOne = a.passed - a.year;
+        const invetorTwo = b.passed - b.year;
 
+        return inventorOne > inventorTwo ? return -1 : return 1;
+      })
+
+      console.table(oldest)
 
     // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
     // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
