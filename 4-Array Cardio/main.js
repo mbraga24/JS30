94e981d6e2703d5d47d5d4427bc75bd840fee6f7
Marlon Braga <rdg.brg@gmail.com>
2019-05-05 12:23:28 -0400

    Cardio

--- 4-Array Cardio/main.js
+++ 4-Array Cardio/main.js
@@ -13,6 +13,7 @@ const inventors = [
       { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
     ];
     const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
         // const fifteen = inventors.filter(function(inventor) {
@@ -29,7 +30,7 @@ const inventors = [
         // const name = inventors.map((inventor) => {
         //   return `${inventor.first} ${inventor.last}`;
         // });
-        //
+
         // console.table(name);
 
     // Array.prototype.sort()
@@ -75,6 +76,13 @@ const inventors = [
 
     // 7. sort Exercise
     // Sort the people alphabetically by last name
+    const alpha = people.sort((lastOne, nextOne) => {
+      const [aLast, aFirst] = lastOne.split(', ');
+      const [bLast, bFirst] = nextOne.split(', ');
+
+      return aLast > bLast ? 1 : -1;
+    });
+    console.log(alpha)
 
 
     // 8. Reduce Exercise
