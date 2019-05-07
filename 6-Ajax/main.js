73fea9036ce60448abe78d71a1ca4da4856368a2
Marlon Braga <rdg.brg@gmail.com>
2019-05-06 22:12:44 -0400

    6-Ajax

--- 6-Ajax/main.js
+++ 6-Ajax/main.js
@@ -1,1 +1,39 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+// USING FETCH
+fetch(endpoint)
+  .then((blob) => blob.json())
+  .then((data) => cities.push(...data)) // Spread into the push() method
+
+function findMatches(wordToMatch, cities) {
+
+  return cities.filter(place => {
+
+    // This block of code will filter if the city or state matches what
+    // was searched.
+    const regex = new RegExp(wordToMatch, 'gi'); // g: global - i: insensitive
+    return place.city.match(regex) ||  place.state.match(regex)
+
+  });
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray.map(place => {
+    return `
+      <li>
+        <span class='name'>${place.city}, ${place.state}</span>
+        <span class='population'>${place.population}</span>
+      </li>
+      `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
