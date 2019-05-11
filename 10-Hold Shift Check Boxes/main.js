3a103386adb9b0e112548b2394c5a1bd818599df
Marlon Braga <rdg.brg@gmail.com>
2019-05-11 00:41:15 -0400

    JS30 - Hold Shift Check Boxes

--- 10-Hold Shift Check Boxes/main.js
+++ 10-Hold Shift Check Boxes/main.js
@@ -5,7 +5,13 @@ let lastChecked;
 function handleCheck(event) {
   // Check if the user has the shift key down
   // AND check if they are checking it.
-  
+  let inBetween = false;
+  if (event.shiftKey && this.checked) {
+
+    // Loop over every single checkbox.
+    checkboxes.forEach(checkbox => {
+      
+    })
 
   }
 
