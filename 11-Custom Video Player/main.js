053024ff0766e901c2f95c93163c558a7150cde0
Marlon Braga <rdg.brg@gmail.com>
2019-05-14 23:45:12 -0400

    Custom Video Player

--- 11-Custom Video Player/main.js
+++ 11-Custom Video Player/main.js
@@ -19,13 +19,18 @@ function togglePlay() {
 }
 
 function updateButton() {
-  const icon = this.paused ? '►' : '❚ ❚';
+  const icon = this.paused  ? '►' : '❚ ❚';
   toggle.textContent = icon;
   console.log(icon)
 }
 
+function skip() {
+  // console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
