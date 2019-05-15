efd4bcbb0b701ea8d59cbfc576bd7597558ba202
Marlon Braga <rdg.brg@gmail.com>
2019-05-14 23:57:42 -0400

    Custom Video Player

--- 11-Custom Video Player/main.js
+++ 11-Custom Video Player/main.js
@@ -44,6 +44,7 @@ function handleProgress() {
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
