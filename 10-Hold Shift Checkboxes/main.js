60cf72f175d94eb8273d38bf09031ce3c1aed035
Marlon Braga <rdg.brg@gmail.com>
2019-06-24 18:00:02 -0400

    JS30 Tweaks

--- 10-Hold Shift Checkboxes/main.js
+++ 10-Hold Shift Checkboxes/main.js
@@ -2,22 +2,14 @@ const checkboxes = document.querySelectorAll(".inbox input[type='checkbox']");
 
 let lastChecked;
 
-function handleCheck(event) {
-  // Check if the user has the shift key down
-  // AND check if they are checking it.
+function handleCheck() {
   let inBetween = false;
   if (event.shiftKey && this.checked) {
 
-    // Loop over every single checkbox.
     checkboxes.forEach(checkbox => {
-      // Will change the value of 'inBetween' twice when shiftkey was held
-      // and 2 distinct boxes checked.
       if (checkbox === this || checkbox === lastChecked) {
         inBetween = !inBetween;
       }
-
-      // Will check all the boxes in between the two checkboxes evaluated
-      // in the previous conditional.
       if (inBetween) {
         checkbox.checked = true;
       }
