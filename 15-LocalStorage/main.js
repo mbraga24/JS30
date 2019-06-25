ebdf441557431010a7acc7f8cfe3aabed943f69e
Marlon Braga <rdg.brg@gmail.com>
2019-06-24 21:32:01 -0400

    15-LocalStorage / Add localStorage JSON.stringy() and JSON.parse()

--- 15-LocalStorage/main.js
+++ 15-LocalStorage/main.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
 function addItem(e) {
   e.preventDefault();
@@ -31,3 +31,5 @@ function populateList(plates = [], plateList) {
 }
 
 addItems.addEventListener('submit', addItem);
+
+populateList(items, itemsList)
