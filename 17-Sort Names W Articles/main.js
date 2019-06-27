e7221df186543694c2fd7c6d86d3f7f22e12a88f
Marlon Braga <rdg.brg@gmail.com>
2019-06-27 17:33:29 -0400

    17-Sort Names W Articles

--- 17-Sort Names W Articles/main.js
+++ 17-Sort Names W Articles/main.js
@@ -1,1 +1,30 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = ['The Red Hot chili peppers', 'Nirvana', 'Queens', 'Guns and Roses ', 'The Cure', 'Jack Johnson', 'The Beautiful Girls', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const sorted = document.querySelector("#bands")
+
+function strip(bandName) {
+  return bandName.replace('/^(the |a |an )/i')
+}
+
+function displayNames(bands) {
+  const html = bands.map(band => {
+    return `
+      <li>
+        ${band}
+      </li>
+    `
+  }).join("");
+  sorted.innerHTML = html;
+}
+
+const sortedBands = bands.sort((a,b) => {
+  if (strip(a) > strip(b)) {
+    return 1;
+  } else {
+    return -1;
+  }
+})
+
+displayNames(sortedBands)
+
+console.log(sortedBands)
