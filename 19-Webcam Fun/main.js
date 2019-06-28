201ddfa6adec57316db34a628fac0357fb1b0fea
Marlon Braga <rdg.brg@gmail.com>
2019-06-28 17:42:41 -0400

    19-Webcam Fun / HTMLMediaElement.srcObject implementation - window.URL.createObjectURL() is no longer used

--- 19-Webcam Fun/main.js
+++ 19-Webcam Fun/main.js
@@ -10,5 +10,21 @@ function getVideo() {
     audio: false
   }
   navigator.mediaDevices.getUserMedia(constraints)
-  
+    .then(localMediaStream => {
+      console.log(localMediaStream)
+      // According to MDN documentation:
+      /*
+        In older versions of the Media Source specification, attaching a stream to a <video> 
+        element required creating an object URL for the MediaStream. This is no longer necessary, 
+        and browsers are removing support for doing this.
+
+        solution for this issue:
+        https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+      */
+      video.srcObject = localMediaStream;
+      video.play()
+    });
+
 }
+
+getVideo()
