66f9097a9b414f4b74fd038a23cbaef16867dbe1
Marlon Braga <rdg.brg@gmail.com>
2019-06-28 02:09:41 -0400

    18-Tally String Times with Reduce

--- 18-Tally String Times with Reduce/main.js
+++ 18-Tally String Times with Reduce/main.js
@@ -1,11 +1,20 @@
 const timeNodes = Array.from(document.querySelectorAll("[data-time]"));
 
 console.log(timeNodes)
-let seconds = timeNodes.map(nodes => {
-  return nodes.dataset.time
-}).map(datatime => {
-   [min, secs] = datatime.split(":").map(parseFloat)
-   return [min, secs]
-}).reduce((total, videoSec) => total + videoSec)
+let seconds = timeNodes
+  .map(node => {
+    return node.dataset.time
+  })
+  .map(datatime => {
+   const [min, secs] = datatime.split(":").map(parseFloat)
+   return (min * 60) + secs;
+}).reduce((total, videoSecond) => total + videoSecond)
 
-console.log(seconds)
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft %  3600;
+
+const minutes = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, minutes, secondsLeft)
