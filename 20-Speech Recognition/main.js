b9fe1a04978335909dad8deacb3c354baa113bf7
Marlon Braga <rdg.brg@gmail.com>
2019-07-05 17:18:44 -0400

    Speech Recognition - Hello, Jarvis

---
+++ 20-Speech Recognition/main.js
@@ -0,0 +1,55 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p')
+const words = document.querySelector('.words')
+words.appendChild(p);
+
+function greeting(responses, index) {
+  let p = document.createElement('p');
+  p.innerHTML = responses[index]
+  let lastChild = words.lastChild
+  lastChild.appendChild(p)
+}
+
+function fetchWeather(responses, index) {
+  let p = document.createElement('p');
+  p.innerHTML = responses[index];
+  let lastChild = words.lastChild
+  lastChild.appendChild(p)
+}
+
+recognition.addEventListener('result', (e) => {
+  // console.log(e.results)
+  const transcript = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript).join("")
+  
+  p.textContent = transcript
+
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
+  if (transcript.includes("hi Jarvis") || transcript.includes("hello Jarvis")) {
+    let responses = ["Hello, sir!", "Hi! How may I help you, sir?"]
+    let index = Math.floor(Math.random() * responses.length)
+
+    greeting(responses, index)
+  } else if (transcript.includes("Jarvis get me the weather") || transcript.includes("Jarvis give me the weather")) {
+    let responses = ["At once, sir!", "Not a problem, sir.", "Not a problem. Hopefully it won't rain. I know how much that upsets you."];
+    let index = Math.floor((Math.random() * responses.length))
+
+    fetchWeather(responses, index)
+  } else if (transcript.includes("what's up Jarvis")) {
+
+  }
+
+  console.log(transcript)
+})
+
+recognition.addEventListener('end', recognition.start)
+
+recognition.start();
